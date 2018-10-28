1cd77a5efb71b2a63d522eda3f934048705708e5
black-monster <ankurraja28@gmail.com>
2018-10-28 10:50:02 +0530

    Added css files

--- src/com/luv2code/springdemo/controller/CustomerController.java
+++ src/com/luv2code/springdemo/controller/CustomerController.java
@@ -4,9 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.luv2code.springdemo.dao.CustomerDAO;
 import com.luv2code.springdemo.entity.Customer;
-import com.luv2code.springdemo.services.CustomerServiceImpl;
+import com.luv2code.springdemo.services.CustomerService;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,11 +16,11 @@ public class CustomerController {
 	
 	// need to inject CustomerDAO
 		@Autowired
-		private CustomerServiceImpl customerServiceImpl;
+		private CustomerService customerService;
 
 		@RequestMapping("/list")
 		public String listCustomer(Model theModel) {
-			List<Customer> theCustomers=customerServiceImpl.getCustomers();
+			List<Customer> theCustomers=customerService.getCustomers();
 			
 			theModel.addAttribute("customers",theCustomers);
 			return "list-customers";
